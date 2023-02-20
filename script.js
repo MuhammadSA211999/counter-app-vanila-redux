ff732444787849c5ae48b42aa30288f756c18ea1
Muhammad SA <MuhammadSA211999>
2023-02-20 23:38:05 +0600

    integrate redux store and reducer func

---
+++ script.js
@@ -0,0 +1,20 @@
+const initialState = {
+    value: 0
+}
+// counter reducer function
+const counterReducer = (state = initialState, action) => {
+    if (action.type === 'increment') {
+        return {
+            ...state,
+            value: state.value + 1
+        }
+    }
+    else if (action.type === 'decrement') {
+        return {
+            ...state,
+            value: state.value - 1
+        }
+    } else {
+        return state
+    }
+}
