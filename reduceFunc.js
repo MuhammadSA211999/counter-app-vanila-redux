5696233c04f953775a3a3f8ac188b3a9ff58c728
Muhammad SA <MuhammadSA211999>
2023-02-22 00:02:07 +0600

    explore reduce func with comments

--- reduceFunc.js
+++ reduceFunc.js
@@ -1,3 +1,8 @@
+/*Reduce function action dispatch hole kebol call hoy
+REDUX er reducer function application er action guluke amader logic onuzayi reduce kore ekti updated single value return kore
+reduce function ti pure function hote hoy
+reduce function ekti object return kore
+ */
 const actions = [
 
     { type: 'inc', payload: 1 },
@@ -6,8 +11,10 @@ const actions = [
     { type: 'dec', payload: 1 },
 ]
 const reducer = (state, action) => {
-    // action hocce actions array er protiti object
+    // state=initialState
     // state hocce result or previous result
+    // action hocce actions array er protiti object
+
     if (action.type === 'inc') {
         return {
             ...state,
@@ -27,5 +34,6 @@ const reducer = (state, action) => {
 const initialState = {
     value: 0
 }
+// initialState ti reducer function er state er moddhe pawa jabe
 const value = actions.reduce(reducer, initialState)
 console.log(value);
